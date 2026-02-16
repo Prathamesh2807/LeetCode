485babe9b8d3cf56cc42bf30e2a480b92099c2b7
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-02-16 23:48:16 +0530

    solved  leetcode 27 remove element

---
+++ 27removeelement.c
@@ -0,0 +1,11 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int k = 0 ;
+    for (int i = 0 ; i<numsSize ; i++){
+
+        if (nums[i] != val){
+            nums[k] = nums[i];
+            k=k+1 ;             // no of ele je rahtil after shifting==deletion
+        }
+    } 
+    return k ;
+}
